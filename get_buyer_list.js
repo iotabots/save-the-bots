d5c5176cb2069a9195cf117d0a3b4464cfee2b3d
huhn511 <sehe89@gmail.com>
2021-11-04 13:57:40 +0100

    add drop list and scripts

---
+++ get_buyer_list.js
@@ -0,0 +1,29 @@
+var fs = require('fs')
+
+/* Connect to ethereum node */
+const etherUrl = "https://evm.wasp.sc.iota.org";
+const abi = require("./iotabots_abi.json")
+var Contract = require('web3-eth-contract');
+
+Contract.setProvider(etherUrl);
+
+
+var contract = new Contract(abi, "0x2f6EFb05Dd92Af605184199296979979379Ecbd3");
+
+async function main() {
+    for (var i = 1; i <= 500; i++) {
+        await contract.methods.ownerOf(i).call({ from: '0x8863ae48646c493efF8cd54f9Ffb8Be89669E62A' }, function (error, result) {
+            var line = i + ":" + result + "\n"
+            fs.appendFile('drop0.txt', line, function (err) {
+                if (err) {
+                  // append failed
+                } else {
+                  // done
+                }
+              })
+        });
+    }
+
+}
+
+main()
